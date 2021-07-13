db30d524cbbd99850c14aa93c162b241eb40c7e5
sagarhasan273 <77304333+sagarhasan273@users.noreply.github.com>
2021-07-13 17:06:00 +0600

    sagar hasan

--- python_code.py
+++ python_code.py
@@ -1,16 +1,9 @@
 def main():
-
-    
     print("Keep it logically awesome.")
-
     f = open("quotes.txt")
-
     quotes = f.readlines()
-
     f.close()
-
     print("there is quotes", quotes)
-
 
 if __name__ == '__main__':
     main()
