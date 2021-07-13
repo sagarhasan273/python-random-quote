f9e2574ba91f0f684af27f0f34b5c8010edc67c9
sagarhasan273 <77304333+sagarhasan273@users.noreply.github.com>
2021-07-13 17:02:24 +0600

    first commit

--- python_code.py
+++ python_code.py
@@ -1,12 +1,16 @@
 def main():
+
+    
     print("Keep it logically awesome.")
 
     f = open("quotes.txt")
+
     quotes = f.readlines()
+
     f.close()
 
     print("there is quotes", quotes)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
