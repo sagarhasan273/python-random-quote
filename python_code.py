7a658206e240597e46104c7ccacdc3962b4ff6a4
sagarhasan273 <77304333+sagarhasan273@users.noreply.github.com>
2021-07-13 17:48:48 +0600

    Update python_code.py

--- python_code.py
+++ python_code.py
@@ -1,9 +1,1 @@
-def main():
-    print("Keep it logically awesome.")
-    f = open("quotes.txt")
-    quotes = f.readlines()
-    f.close()
-    print("there is quotes", quotes)
-
-if __name__ == '__main__':
-    main()
+print("This is my final code!")
